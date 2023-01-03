edee7f8ea6130fef24202f8614e93c7b4b7b75b1
Yasir Alabadi <yasir@MMT-MAC-4396.local>
2023-01-03 16:15:26 +0000

    added cities obj, direction of app changing, no need for api

---
+++ src/data/cities.ts
@@ -0,0 +1,77 @@
+export const cities = [
+    {
+        name: "New York",
+        longitude: -74.0060,
+        latitude: 40.7128
+    },
+    {
+        name: "Los Angeles",
+        longitude: -118.2437,
+        latitude: 34.0522
+    },
+    {
+        name: "Chicago",
+        longitude: -87.6298,
+        latitude: 41.8781
+    },
+    {
+        name: "Houston",
+        longitude: -95.3698,
+        latitude: 29.7604
+    },
+    {
+        name: "Philadelphia",
+        longitude: -75.1652,
+        latitude: 39.9526
+    },
+    {
+        name: "Phoenix",
+        longitude: -112.0740,
+        latitude: 33.4484
+    },
+    {
+        name: "San Antonio",
+        longitude: -98.4936,
+        latitude: 29.4241
+    },
+    {
+        name: "San Diego",
+        longitude: -117.1611,
+        latitude: 32.7157
+    },
+    {
+        name: "Dallas",
+        longitude: -96.7970,
+        latitude: 32.7767
+    },
+    {
+        name: "San Jose",
+        longitude: -121.8863,
+        latitude: 37.3382
+    },
+    {
+        name: "Austin",
+        longitude: -97.7431,
+        latitude: 30.2672
+    },
+    {
+        name: "Jacksonville",
+        longitude: -81.6557,
+        latitude: 30.3322
+    },
+    {
+        name: "Fort Worth",
+        longitude: -97.3307,
+        latitude: 32.7555
+    },
+    {
+        name: "Columbus",
+        longitude: -82.9850,
+        latitude: 39.9612
+    },
+    {
+        name: "San Francisco",
+        longitude: -122.4194,
+        latitude: 37.7749
+    }
+];
